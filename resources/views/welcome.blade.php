4d5b4af43b523d25177d1cd90815140f1636854a
MarceloFreitas <marcelohfreitas235@gmail.com>
2025-10-06 21:42:23 -0300

    Logo da steam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,11 @@
         <div class="row justify-content-center align-items-center" style="height: calc(100vh - 200px);">
             <div class="col-md-8 d-flex justify-content-center flex-column align-items-center">
 
-                <img src="{{ asset('images/logo.png') }}" alt="Nortti Logo" width="400px">
-                <h2 class="text-center text-dark mt-3">Bem-vindo ao <span class="text-primary">Nortti Vendas</span></h2>
+                <img src="{{ asset('images/logo.png') }}" alt="Steam Verde Logo" width="400px">
+                <h2 class="text-center text-dark mt-3">Bem-vindo ao <span class="text-success">Steam Verde</span></h2>
                 <div class="d-flex justify-content-center mt-4 gap-3 w-100">
                     <a href="{{ route('login') }}" class="btn btn-secondary btn-lg w-50">Login como Cliente</a>
-                    <a href="{{ route('admin.login') }}" class="btn btn-primary btn-lg w-50">Login como Administrador</a>
+                    <a href="{{ route('admin.login') }}" class="btn btn-success btn-lg w-50">Login como Administrador</a>
                 </div>
             </div>
         </div>
